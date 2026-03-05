057e2296f7ca3e592b014ce2a7a1b355ddddf239
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-03-05 21:40:12 +0530

    Add files via upload

---
+++ day1.py
@@ -0,0 +1,28 @@
+# #what is python 
+# Python is a popular programming language. It was created by Guido van Rossum, and released in 1991.
+
+# It is used for:
+
+# web development (server-side),
+# software development,
+# mathematics,
+# system scripting.
+
+#use the print function 
+print("Hello, World!")
+print("Hello World!")
+print("I am learning Python.")
+print("It is awesome!")
+
+#comments in python 
+# Comments can be used to explain Python code.
+# Comments can be used to make the code more readable.
+# Comments can be used to prevent execution when testing code.
+
+
+
+#This is a comment
+print("Hello, World!")
+
+
+#python is a case sensitive language
---
+++ variables.py
@@ -0,0 +1,16 @@
+#variables:
+
+# Variables are containers for storing data values.
+x = 5
+y = "John"
+print(x)
+print(y)
+
+#checking the data type:
+
+# type() -method
+
+x = 5
+y = "John"
+print(type(x))
+print(type(y))
