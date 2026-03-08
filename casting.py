405ad291c96cef5f90c58d8f7b7ce7b50e2dcbb7
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-03-08 11:40:35 +0530

    Add files via upload

---
+++ casting.py
@@ -0,0 +1,37 @@
+#Casting: Converting one data type to another data type. We use int(), float(), str(), list
+
+#int 
+x = int(1)   
+y = int(2.8) 
+z = int("3")
+
+print(x)
+print(y)
+print(z)
+
+#float
+x = float(1)     
+y = float(2.8)   
+z = float("3")   
+w = float("4.2") 
+
+print(x)
+print(y)
+print(z)
+print(w)
+
+#strings
+x = str("s1")
+y = str(2)   
+z = str(3.0)
+
+
+print(x)
+print(y)
+print(z)
+
+# str to list
+first_name = 'mahesh'
+print(first_name)        
+first_name_to_list = list(first_name)
+print(first_name_to_list) 
