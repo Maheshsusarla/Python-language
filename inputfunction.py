216c7f31cc9c6f2489cad41566b47e1f266b4328
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-03-08 11:02:33 +0530

    Add files via upload

---
+++ inputfunction.py
@@ -0,0 +1,22 @@
+
+#input() function is used to take input from the user during the execution of a program. The input() function reads a line of text from the user as a "string" and returns that "string". It allows the user to provide input interactively.
+
+#The input() function allows user input.
+
+# Syntax:
+
+# input(prompt)
+#string only strings
+# x = input('Enter your name:')
+# print('Hello, ' + x)
+
+
+#int 
+# x = int(input('Enter number:'))
+# print( x)
+
+
+#float
+# x = float(input('Enter number:'))
+# print( x)
+
