32b5724e66ba177c89c0ba259e84605760c15578
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-03-29 12:15:48 +0530

    Add files via upload

---
+++ tuples.py
@@ -0,0 +1,37 @@
+# Tuples are used to store imultiple items in a single variable.
+
+# -> Tuple is one of 4 built-in data types in Python used to store collections of data,
+# the other 3 are List, Set, and Dictionary, all with different qualities and usage.
+
+#->A tuple is a collection which is ordered and unchangeable.
+
+# Tuples are written with round brackets
+my_tuple=(1,2,3,"hi")
+
+#access
+# print(my_tuple[0])
+
+
+
+#index method
+new=my_tuple.index(1)
+print(my_tuple,new)
+
+# print(my_tuple)
+# print(my_tuple[0])
+
+# my_tuple[0]=67 
+# print(my_tuple)
+
+# new=my_tuple+(5,"hi")
+# print(new)
+
+#len function
+# print(len(my_tuple))
+
+
+#count
+# print(my_tuple.count(1))
+
+
+#but you change first convert list  after
