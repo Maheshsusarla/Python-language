6ce0c1b0c1e51a10e250127cfc835a2b0a1d95e1
Mahesh Susarla <maheshsusarla5@gmail.com>
2026-03-30 10:38:33 +0530

    Add files via upload

---
+++ set.py
@@ -0,0 +1,28 @@
+#set
+# s={1,2,3}
+# print(s,type(s))
+
+# print(s[0]) error
+
+
+#duplicates not allow
+s={1,2,3,4,1,2,3,4}
+# print(s)
+
+
+#add only one item
+# s.add("mahesh")
+# print(s)
+
+#update add multiple item
+# s.update(["mahesh",543])
+# print(s)
+
+
+#delete
+# s.remove(1)
+# print(s)
+
+
+for n in s:
+    print(n)
